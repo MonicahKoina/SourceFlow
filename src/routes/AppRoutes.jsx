ad5f0721e179117fe03a382f64152ce5d0ac6811
Monicah Koina <monicahkoina1@gmail.com>
2025-07-22 23:30:56 +0300

    routing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Requests from "../pages/needs/Requests";
 
 function AppRoutes() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Requests />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default AppRoutes;
