c78cbd6c5eca48922065f5d415d09075980ba9c2
Monicah Koina <monicahkoina1@gmail.com>
2025-07-25 00:02:33 +0300

    store user credentials in local storage with basic form validation

--- src/pages/user/Register.jsx
+++ src/pages/user/Register.jsx
@@ -5,10 +5,42 @@ import {
 } from "@ant-design/icons";
 import { Button, Card } from "antd";
 import Input from "antd/es/input/Input";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 
 function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLatName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !firstName || !lastName || !password) {
+      alert("All fields are required");
+      return;
+    }
+
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      password,
+    };
+    let savedUsers = JSON.parse(localStorage.getItem("savedUsers")) || [];
+    const emailExists = savedUsers.some((user) => user.email === email);
+    if (emailExists) {
+      alert("Email already exists");
+      return;
+    }
+    savedUsers.push(newUser);
+    localStorage.setItem("savedUsers", JSON.stringify(savedUsers));
+    setFirstName("");
+    setLatName("");
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white  flex items-center justify-center  px-4">
       <Card className=" h-1/4 w-full max-w-md p-6 shadow-lg ">
@@ -17,23 +49,48 @@ function Register() {
           <div className="flex gap-6">
             <div>
               <label htmlFor="firstName">First Name</label>
-              <Input type="text" placeholder="Monicah" name="firstName" />
+              <Input
+                type="text"
+                placeholder="Monicah"
+                name="firstName"
+                required
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </div>
             <div>
               <label htmlFor="lastName">Last Name:</label>
-              <Input type="text" name="lastName" placeholder="Wanjiru"></Input>
+              <Input
+                type="text"
+                name="lastName"
+                placeholder="Wanjiru"
+                required
+                value={lastName}
+                onChange={(e) => setLatName(e.target.value)}
+              />
             </div>
           </div>
           <label htmlFor="email"> Email:</label>
-          <Input type="email" placeholder="accountName@gmail.com" />
+          <Input
+            type="email"
+            placeholder="accountName@gmail.com"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label htmlFor="password">Password:</label>
           <Input.Password
             placeholder="input password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             iconRender={(visible) =>
               visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
             }
           />
-          <Button type="primary">Create Account</Button>
+          <Button type="primary" onClick={handleSubmit}>
+            Create Account
+          </Button>
         </form>
         <h3 className="mt-4">
           Already have an account? <Link to={"/login"}>login</Link>
