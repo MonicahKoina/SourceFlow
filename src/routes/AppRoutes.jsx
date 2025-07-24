b7796161c0a47ca737c3d4c4579129c1d6171126
Monicah Koina <monicahkoina1@gmail.com>
2025-07-24 17:52:34 +0300

    routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import Requests from "../pages/needs/Requests";
+import Home from "../pages/Hero";
+import Register from "../pages/user/Register";
+import Login from "../pages/user/Login";
 
 function AppRoutes() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Requests />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
